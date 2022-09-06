9798492a5d74b48ea31cf14e3c0a6a580266e41a
igorleb <ip.lebedev07@gmail.com>
2022-09-06 16:23:06 +0400

    feat: add information

--- src/components/elements/Main/Information.jsx
+++ src/components/elements/Main/Information.jsx
@@ -1,9 +1,40 @@
-import styles from './Main.module.scss'
+import Button from '../../UI/Button/Button';
+import styles from './Main.module.scss';
 
-const Information = ({movie}) => {
+const Information = ({ movie }) => {
+  const addToFavorites = movieName => {
+    let favorites = localStorage.getItem('favMovies');
+    if (favorites) {
+      favorites = JSON.parse(favorites);
+      localStorage.setItem('favMovies', [...favorites, movieName]);
+      alert(`${movieName} теперь в избранном!`);
+    }
+  };
+
   return (
-    <div>Information</div>
-  )
-}
+    <div className={styles.info}>
+      <img src={movie.logo} alt={movie.name} width='200' />
 
-export default Information
+      <div className={styles.additional}>
+        <span>{movie.year}</span>
+        <span>{movie.limitAge}</span>
+        <span>{movie.rating}</span>
+        <span>{movie.duration}</span>
+      </div>
+
+      <div className={styles.description}>{movie.description}</div>
+      <div className={styles.buttons}>
+        <Button cb={() => console.log('video is open')}>
+          <i className='bx bx-play'></i>
+          <span>Play</span>
+        </Button>
+        <Button cb={addToFavorites}>
+          <i className='bx bx-play'></i>
+          <span>+ My list</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Information;
