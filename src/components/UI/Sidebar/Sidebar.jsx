9ce3f5e2e2cf7d310d1395c0e347f9065138c4ff
igorleb <ip.lebedev07@gmail.com>
2022-09-07 21:41:10 +0400

    feat: add background image

--- src/components/UI/Sidebar/Sidebar.jsx
+++ src/components/UI/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ const menu = ['Popular', 'TV Shows', 'Films', 'My list'];
 
 const Sidebar = ({ isSidebarShow }, setIsSidebarShow) => {
   return (
-    <div className={styles.sidebar}>
+    <div className={styles.sidebar} style={{ width: isSidebarShow ? 30 : 15 }}>
       <button onClick={() => setIsSidebarShow(true)}>
         <i className={`bx bx-${isSidebarShow ? 'x' : 'borer-left'}`}></i>
       </button>
--- src/components/elements/Main/Main.jsx
+++ src/components/elements/Main/Main.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { DATA } from '../../../data';
 import BottomNavigation from '../../UI/BottomNavigation/BottomNavigation';
 import Information from './Information';
@@ -5,10 +6,19 @@ import Sidebar from '../../UI/Sidebar/Sidebar';
 import styles from './Main.module.scss';
 
 const Main = () => {
+  const [isSidebarShow, setIsSidebarShow] = useState(false);
   return (
     <div>
-      <Sidebar />
-      <div style={{}}>
+      <Sidebar
+        isSidebarShow={isSidebarShow}
+        setIsSidebarShow={setIsSidebarShow}
+      />
+      <div
+        style={{
+          backgroundImage: `url(${DATA[0].mainImage})`,
+          width: isSidebarShow ? 70 : 85,
+        }}
+      >
         <Information movie={DATA[0]} />
         <BottomNavigation />
       </div>
