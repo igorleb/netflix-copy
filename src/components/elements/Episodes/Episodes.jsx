84a16f067d084434b8c3d4347509d726be83ad80
igorleb <ip.lebedev07@gmail.com>
2022-09-18 22:04:48 +0400

    feat: add episodes

--- src/components/elements/Episodes/Episodes.jsx
+++ src/components/elements/Episodes/Episodes.jsx
@@ -1,5 +1,20 @@
+import eternals from '../../../images/eternals.webp';
+import iron_man from '../../../images/iron_man.webp';
+import spider from '../../../images/spider.webp';
+import black_widow from '../../../images/black_widow.webp';
+import styles from './Episodes.module.scss';
+
 const Episodes = () => {
-  return <div>Episodes</div>;
+  return (
+    <div className={styles.slider}>
+      <div className={styles.sliderImg}>
+        <img src={eternals} alt={eternals} width='20%' />
+        <img src={iron_man} alt={iron_man} width='20%' />
+        <img src={spider} alt={spider} width='20%' />
+        <img src={black_widow} alt={black_widow} width='20%' />
+      </div>
+    </div>
+  );
 };
 
 export default Episodes;
--- src/components/elements/Main/Main.jsx
+++ src/components/elements/Main/Main.jsx
@@ -4,11 +4,12 @@ import BottomNavigation from '../../UI/BottomNavigation/BottomNavigation';
 import Information from './Information';
 import Sidebar from '../../UI/Sidebar/Sidebar';
 import styles from './Main.module.scss';
+import Episodes from '../Episodes/Episodes';
 
 const Main = () => {
   const [isSidebarShow, setIsSidebarShow] = useState(false);
   const [activeTab, setActiveTab] = useState(1);
-  
+
   return (
     <div className={styles.wrapper}>
       <Sidebar
@@ -21,10 +22,14 @@ const Main = () => {
           backgroundImage: `url(${DATA[0].mainImage})`,
           width: isSidebarShow ? '85%' : '90%',
         }}
-      > 
-        <Information movie={DATA[0]} />
-        <BottomNavigation activeTab={activeTab} setActiveTab={setActiveTab}/>
+      >
+        {activeTab === 1 ? (
+          <Information movie={DATA[0]} />
+        ) : (
+          activeTab === 2 && <Episodes />
+        )}
       </div>
+      <BottomNavigation activeTab={activeTab} setActiveTab={setActiveTab} />
     </div>
   );
 };
